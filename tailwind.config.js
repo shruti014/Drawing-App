660005b351359d16f12e0f5bca2b2d4ecd4fdb31
Shruti Sinha <sshrutisinha0@gmail.com>
2025-05-14 21:19:58 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        xs: { max: "500px" },
+        sx: { max: "900px" },
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        "roboto-condensed": ["Roboto Condensed", "sans-serif"],
+        "roboto-slab": ["Roboto Slab", "serif"],
+        poppins: ["Poppins", "sans-serif"],
+        "courier-prime": ["Courier Prime", "monospace"],
+        exo: ["Exo", "sans-serif"],
+        lora: ["Lora", "serif"],
+        barlow: ["Barlow", "sans-serif"],
+        "open-sans": ["Open Sans", "sans-serif"],
+        lato: ["Lato", "sans-serif"],
+        merriweather: ["Merriweather", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
